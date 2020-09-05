10392c4ca96900edf74638a86170ef3f743793cf
Sunny <gustjs1013@naver.com>
2020-09-05 15:04:52 +0900

    first commit

---
+++ exercise-spring1/exercise-spring1/src/main/java/cadispring/exercisespring1/ExerciseSpring1Application.java
@@ -0,0 +1,13 @@
+package cadispring.exercisespring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseSpring1Application.class, args);
+	}
+
+}
